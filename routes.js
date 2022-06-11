ee6e14ffcaef099c4df5d00728dc41cd8dc0f424
mvicentejr <marcelo_vicentejr@yahoo.com.br>
2022-06-11 14:04:23 -0300

    Primeiro Commit

---
+++ routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const route = express.Router();
+
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+
+// Rotas da Home
+route.get('/', homeController.index);
+
+//Rotas de Login
+route.get('/login/index', loginController.index);
+
+module.exports = route;
