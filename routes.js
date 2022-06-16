73e0f0153adb72314c027e7e91d9bdb215f526d1
mvicentejr <marcelo_vicentejr@yahoo.com.br>
2022-06-16 15:12:44 -0300

    Login usuario

--- routes.js
+++ routes.js
@@ -9,5 +9,6 @@ route.get('/', homeController.index);
 
 //Rotas de Login
 route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
