658085cc3b78915262cd320f42ead5ac9d7f68aa
mvicentejr <marcelo_vicentejr@yahoo.com.br>
2022-06-27 20:17:04 -0300

    Criar contato

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
 
 // Rotas da Home
 route.get('/', homeController.index);
@@ -10,5 +13,12 @@ route.get('/', homeController.index);
 //Rotas de Login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+//Rotas de Contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
 
 module.exports = route;
