87560113d0cf6c0e48f6762f92669946be0135b5
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2022-06-30 20:34:29 -0300

    Crud finalizado

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -6,7 +6,7 @@ exports.index = (req, res) => {
   });
 };
 
-exports.register = async(req, res) => {
+exports.register = async (req, res) => {
   try {
   const contato = new Contato(req.body);
   await contato.register();
@@ -27,7 +27,7 @@ exports.register = async(req, res) => {
 
 };
 
-exports.editIndex = async (req, res) => {
+exports.editIndex = async function (req, res) {
   if(!req.params.id) return res.render('404');
 
   const contato = await Contato.buscaPorId(req.params.id);
@@ -35,4 +35,37 @@ exports.editIndex = async (req, res) => {
   if(!contato) return res.render('404');
 
   res.render('contato', { contato });
+};
+
+exports.edit = async function (req, res) {
+  try {
+    if(!req.params.id) return res.render('404');
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
+  
+    if(contato.errors.length > 0) {
+      req.flash('errors', contato.errors);    
+      req.session.save(() => res.redirect('/contato/index'));
+      return;
+    }
+  
+    req.flash('success', 'Contato alterado com sucesso');    
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+    return;
+    } catch(e) {
+      console.log(e);
+      return res.render('404');
+    }    
+};
+
+exports.delete = async function(req, res) {
+  if(!req.params.id) return res.render('404');
+
+  const contato = await Contato.delete(req.params.id);
+
+  if(!contato) return res.render('404');
+
+  req.flash('success', 'Contato apagado com sucesso');    
+    req.session.save(() => res.redirect('back'));
+    return;
 };
